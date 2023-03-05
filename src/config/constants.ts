fef40570797cf0bf69e56d103ac137680f4a6842
Daniel Soares <daniel.soares@me.com>
2023-03-05 06:54:08 -0300

    fix: mail and description

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,8 +2,9 @@ export const constants = {
   SITE_NAME: 'Brawo Impact',
   SITE_URL: 'https://brawo-impact.fr',
   // TODO: Replace description with a real one
-  SITE_DESCRIPTION: 'Lorem ipsum',
+  SITE_DESCRIPTION:
+    "Pour s'occuper sérieusement de notre planète, il faut certes fixer des objectifs, mais il faut aussi des gens pour innover, pour construire et pour les atteindre. Nous devons rapidement produire autrement, et nous avons pour cela besoin de développer des nouveaux savoir-faire et des nouveaux des savoir-être. Pour relever ce défi, il faut anticiper les besoins, imaginer les métiers de demain, identifier, former et recruter. Parce que la transition environnementale nous oblige à repenser les métiers et les compétences, Brawo a créé, Brawo Impact, le service de recrutement 100% spécialisé dans les métiers de la transformation durable.",
   NEWS_LIMIT: 5,
-  CONTACT_FORM_EMAIL_TO: 'daniel.soares@me.com',
+  CONTACT_FORM_EMAIL_TO: 'kaelig.sadaune@brawo.fr',
   CONTACT_FORM_EMAIL_FROM: 'kaelig.sadaune@brawo.fr'
 }
